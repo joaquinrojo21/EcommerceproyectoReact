aa2bd9f69d7e3f383e2eba727df5d76f97a1ab36
Joaquin Rojo <rojo.joaquin88@gmail.com>
2023-02-16 11:42:36 -0300

    ultimos detalles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,9 +13,7 @@ const Navbar = () => {
 </button>
 <div className="collapse navbar-collapse" id="navbarNav">
   <ul className="navbar-nav">
-    <li className="nav-item">
-      <a className="nav-link active" aria-current="page" href="#"> Inicio</a>
-    </li>
+    
     <div className="dropdown">
   <button className="btn btn-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
     Categorias
